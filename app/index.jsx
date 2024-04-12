d8d5e1814b189c2f2787f41e49f8c1b6f6b22a81
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2024-04-13 00:50:18 +0530

    Setup Font

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">VIDEO WIZ</Text>
+      <Text className="text-3xl font-pblack">VIDEO WIZ</Text>
       <StatusBar style="auto" />
     </View>
   );
