a70a954566198ec977497aa8e2316df31b3ca7f3
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2024-04-13 00:38:50 +0530

    Project Setup

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,18 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
 import { Slot } from "expo-router";
-import React from "react";
 
 const RootLayout = () => {
   return <Slot />;
 };
 
 export default RootLayout;
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
