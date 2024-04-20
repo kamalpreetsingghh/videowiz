5af9a665d785fcde963a03c19651259391d0ace8
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2024-04-21 02:07:50 +0530

    Code Refactor

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,7 +1,4 @@
-import { View, Text, Image, TouchableOpacity, Alert } from "react-native";
-import { useState } from "react";
-import { icons } from "../constants";
-import { updateFavorites } from "../lib/appwrite";
+import { View, Text, Image } from "react-native";
 import VideoPlayer from "./VideoPlayer";
 
 const VideoCard = ({
@@ -17,50 +14,6 @@ const VideoCard = ({
   user,
   isProfileScreen = false,
 }) => {
-  const [play, setPlay] = useState(false);
-  const [isFavorite, setIsFavorite] = useState(false);
-
-  let isFavoriteVideo = false;
-
-  if (user) {
-    const userWithLikedVideo = likedUsers.find(
-      (likedUser) => likedUser.$id === user.$id
-    );
-    if (userWithLikedVideo) {
-      isFavoriteVideo = true;
-    }
-  }
-
-  const onFavoriteClick = async () => {
-    if (isFavoriteVideo) {
-      const updatedUsers = likedUsers.filter(
-        (likedUser) => likedUser.$id !== user.$id
-      );
-      await updateFavorites(updatedUsers, videoId);
-    } else {
-      const updatedUsers = [...likedUsers, user];
-      await updateFavorites(updatedUsers, videoId);
-    }
-  };
-
-  const onDeleteClick = async () => {
-    console.log("Delete Function");
-    console.log(videoId);
-    console.log(creatorId);
-    Alert.alert("Delete Video", "Are you sure you want to delete this video?", [
-      {
-        text: "Cancel",
-        onPress: () => {},
-        style: "cancel",
-      },
-      {
-        text: "Delete",
-        onPress: async () => await deletePost(videoId),
-        style: "destructive",
-      },
-    ]);
-  };
-
   return (
     <View className="flex flex-col items-center px-4 mb-10">
       <View className="flex flex-row gap-3 items-start">
@@ -92,41 +45,18 @@ const VideoCard = ({
             )}
           </View>
         </View>
-
-        <View className="flex flex-row gap-x-4 items-center justify-center h-full">
-          {user && (
-            <TouchableOpacity onPress={() => onFavoriteClick()}>
-              <Image
-                source={icons.favorite}
-                className="w-6 h-6"
-                resizeMode="contain"
-                tintColor={isFavoriteVideo ? "#dc2626" : "#4b5563"}
-              />
-            </TouchableOpacity>
-          )}
-
-          {isProfileScreen && (
-            <TouchableOpacity onPress={() => onDeleteClick()}>
-              <Image
-                source={icons.deleteIcon}
-                className="w-6 h-6"
-                resizeMode="contain"
-                tintColor="#dc2626"
-              />
-            </TouchableOpacity>
-          )}
-        </View>
       </View>
 
       <VideoPlayer
-        play={play}
-        setPlay={(value) => setPlay(value)}
         videoId={videoId}
         title={title}
         prompt={prompt}
         thumbnail={thumbnail}
         video={video}
+        user={user}
         isLoggedUserVideo={user ? user.$id === creatorId : false}
+        isProfileScreen
+        likedUsers={likedUsers}
       />
     </View>
   );
--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -1,78 +1,160 @@
-import { TouchableOpacity, Image } from "react-native";
+import { TouchableOpacity, Image, View, Alert } from "react-native";
 import { ResizeMode, Video } from "expo-av";
 import { icons } from "../constants";
 import { router } from "expo-router";
+import { useState } from "react";
+import { deletePost, updateFavorites } from "../lib/appwrite";
 
 const VideoPlayer = ({
-  play,
-  setPlay,
   videoId,
   title,
   prompt,
   thumbnail,
   video,
   showInfo = true,
+  user = null,
   isLoggedUserVideo = false,
+  isProfileScreen,
+  likedUsers,
 }) => {
-  if (play)
-    return (
-      <Video
-        source={{ uri: video }}
-        className="w-full h-60 rounded-xl mt-1"
-        resizeMode={ResizeMode.CONTAIN}
-        useNativeControls
-        shouldPlay
-        onPlaybackStatusUpdate={(status) => {
-          if (status.didJustFinish) {
-            setPlay(false);
-          }
-        }}
-      />
-    );
+  const [play, setPlay] = useState(false);
+
+  const isFavorite = () => {
+    let isFavoriteVideo = false;
+
+    if (user) {
+      const userWithLikedVideo = likedUsers.find(
+        (likedUser) => likedUser.$id === user.$id
+      );
+      if (userWithLikedVideo) {
+        isFavoriteVideo = true;
+      }
+    }
+
+    return isFavoriteVideo;
+  };
+
+  let isFavoriteVideo = isFavorite();
+
+  const onFavoriteClick = async () => {
+    if (isFavoriteVideo) {
+      const updatedUsers = likedUsers.filter(
+        (likedUser) => likedUser.$id !== user.$id
+      );
+      await updateFavorites(updatedUsers, videoId);
+    } else {
+      const updatedUsers = [...likedUsers, user];
+      await updateFavorites(updatedUsers, videoId);
+    }
+  };
+
+  const onInfoClick = () => {
+    router.push({
+      pathname: `/info/${videoId}`,
+      params: {
+        title,
+        prompt,
+        thumbnail,
+        video,
+        isLoggedUserVideo,
+      },
+    });
+  };
+
+  const onDeleteClick = async () => {
+    Alert.alert("Delete Video", "Are you sure you want to delete this video?", [
+      {
+        text: "Cancel",
+        onPress: () => {},
+        style: "cancel",
+      },
+      {
+        text: "Delete",
+        onPress: async () => await deletePost(videoId),
+        style: "destructive",
+      },
+    ]);
+  };
 
   return (
-    <TouchableOpacity
-      activeOpacity={0.7}
-      onPress={() => setPlay(true)}
-      className="w-full h-60 rounded-xl mt-1 relative flex justify-center items-center"
-    >
-      <Image
-        source={{ uri: thumbnail }}
-        className="w-full h-full rounded-xl mt-3"
-        resizeMode="cover"
-      />
-
-      <Image
-        source={icons.play}
-        className="w-12 h-12 absolute"
-        resizeMode="contain"
-      />
-
-      {showInfo && (
+    <>
+      {play ? (
+        <Video
+          source={{ uri: video }}
+          className="w-full h-60 rounded-xl mt-1"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
+      ) : (
         <TouchableOpacity
-          onPress={() =>
-            router.push({
-              pathname: `/info/${videoId}`,
-              params: {
-                title,
-                prompt,
-                thumbnail,
-                video,
-                isLoggedUserVideo,
-              },
-            })
-          }
-          className="absolute top-0 right-0 mt-4 mr-3 bg-white rounded-full"
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+          className="w-full h-60 rounded-xl mt-1 relative flex justify-center items-center"
         >
           <Image
-            source={icons.info}
-            className="w-6 h-6"
-            resizeMode="contain"
-            tintColor="#000000"
+            source={{ uri: thumbnail }}
+            className="w-full h-full rounded-xl mt-3"
+            resizeMode="cover"
           />
+
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+
+          <View className="flex gap-y-2 absolute p-1 top-0 right-0 mt-1 mr-1">
+            {user && (
+              <TouchableOpacity
+                onPress={onFavoriteClick}
+                className="p-1 bg-white rounded-full"
+              >
+                <Image
+                  source={icons.favorite}
+                  className="w-5 h-5"
+                  resizeMode="contain"
+                  tintColor={isFavoriteVideo ? "#dc2626" : "#000000"}
+                />
+              </TouchableOpacity>
+            )}
+
+            {isProfileScreen && (
+              <TouchableOpacity
+                onPress={() => onDeleteClick()}
+                className="p-1 bg-white rounded-full"
+              >
+                <Image
+                  source={icons.deleteIcon}
+                  className="w-5 h-5"
+                  resizeMode="contain"
+                  tintColor="#dc2626"
+                />
+              </TouchableOpacity>
+            )}
+          </View>
+
+          {showInfo && (
+            <TouchableOpacity
+              onPress={onInfoClick}
+              className="absolute bottom-0 right-0 mr-2 bg-white rounded-full"
+            >
+              <Image
+                source={icons.info}
+                className="w-6 h-6"
+                resizeMode="contain"
+                tintColor="#000000"
+              />
+            </TouchableOpacity>
+          )}
         </TouchableOpacity>
       )}
-    </TouchableOpacity>
+    </>
   );
 };
 
